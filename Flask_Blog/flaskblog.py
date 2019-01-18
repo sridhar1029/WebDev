c44d2efeb9b2f2e4c7c5f6004b4630b37836b88b
Sridhar <adhikarla2sridhar@yahoo.com>
2019-01-18 02:03:30 +0100

    Started with the project

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,14 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Sridhar",
+        "title": "Blog Post 1",
+        "content": "Some random text for bp 1",
+        "date_posted": "April 25, 2018"
+    },
+    {
+        "author": "Sneha",
+        "title": "Blog Post 2",
+        "content": "Some random text for bp 2",
+        "date_posted": "August 9, 2018"
+    },
+    {
+        "author": "Lobo",
+        "title": "Blog Post 3",
+        "content": "Some random text for bp 3",
+        "date_posted": "Dec 31, 2018"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page! Says hello to you :)</h1>"
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
